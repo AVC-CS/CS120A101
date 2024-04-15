f404409692d9e6a166e0ced99b2621beb20deafc
Kayw Lee <kwooler@gmail.com>
2024-04-15 12:10:10 -0700

    A101

--- main.cpp
+++ main.cpp
@@ -8,11 +8,11 @@ int main()
 
 	srclen = mycstrlen(source);
 	cout << "The length of source string " << srclen << endl;
-	mycstrcpy(source, target);
+	mycstrcpy(target, source);
 	tgtlen = mycstrlen(target);
 	cout << "The length of target string " << tgtlen << endl;
 	cout << "The Target String is " << target << endl;
-	srclen = mycstrcat(source, target);
+	srclen = mycstrcat(target, source);
 	cout << "The length of source string " << srclen << endl;
 	cout << "The Concatenated String is " << source << endl;
 }
